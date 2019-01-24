cd02c733ce04a5135f56076e94f00690476f0786
Alexandr Popovich <alexandr.popovih@pushapp.ru>
2019-01-24 18:14:07 +0300

    add show error and loading in orderslistview

--- app/src/main/java/com/aleksdark/taxiapp/presentation/screens/fragmentorderslist/OrdersListView.kt
+++ app/src/main/java/com/aleksdark/taxiapp/presentation/screens/fragmentorderslist/OrdersListView.kt
@@ -1,23 +1,31 @@
 package com.aleksdark.taxiapp.presentation.screens.fragmentorderslist
 
+import android.view.View
+import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.aleksdark.taxiapp.R
 import com.aleksdark.taxiapp.presentation.adapter.OrdersAdapter
 import com.aleksdark.taxiapp.presentation.models.OrderModel
 import com.aleksdark.taxiapp.presentation.mvp.BaseView
 import com.aleksdark.taxiapp.presentation.screens.fragmentordersdetails.OrderDetailsView
+import kotlinx.android.synthetic.main.error_layout.*
 import kotlinx.android.synthetic.main.fragment_active_orders_list.*
+import kotlinx.android.synthetic.main.loading_layout.*
 
 class OrdersListView : BaseView<OrdersListPresenter>(), Contract.View {
 
     var adapter = OrdersAdapter()
 
     override fun showError(isShowing: Boolean) {
-
+        error_layout.visibility = showView(isShowing)
     }
 
     override fun showLoading(isShowing: Boolean) {
+        loading_layout.visibility = showView(isShowing)
+    }
 
+    private fun showView(isShowing: Boolean) : Int{
+       return if (isShowing) View.VISIBLE else View.GONE
     }
 
     override fun getLayoutId(): Int {
@@ -30,6 +38,8 @@ class OrdersListView : BaseView<OrdersListPresenter>(), Contract.View {
 
     override fun initView() {
         rv.layoutManager = LinearLayoutManager(context)
+        val divider = DividerItemDecoration(context, DividerItemDecoration.VERTICAL)
+        rv.addItemDecoration(divider)
         adapter.onItemClick = {
             mPresenter?.onOrderClick(it)
         }
@@ -45,5 +55,4 @@ class OrdersListView : BaseView<OrdersListPresenter>(), Contract.View {
     override fun goToFragment(order: OrderModel) {
         goToFragment(OrderDetailsView.newInstance(order))
     }
-
 }
